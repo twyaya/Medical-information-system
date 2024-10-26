197034df6a85c31e62d2fb737d34357641fc98f6
twyaya <otis8496@gmail.com>
2024-10-26 20:55:09 +0800

    fix

--- appointments/models.py
+++ appointments/models.py
@@ -5,12 +5,14 @@ class Patient(models.Model):
     dob = models.DateField()  # Date of birth
     phone = models.CharField(max_length=15)
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
-    age = models.PositiveIntegerField()
+    age = models.IntegerField(null=False, default=18)
+
 
 class Doctor(models.Model):
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
-    age = models.PositiveIntegerField()
+    age = models.IntegerField(null=False, default=18)
+
 
 class Appointment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
