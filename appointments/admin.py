9830b9d1fece94184415e863f8107e09ce7f49db
twyaya <otis8496@gmail.com>
2024-10-26 20:33:00 +0800

    註冊醫生

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Patient, Appointment
+from .models import Patient, Appointment, Doctor
 
 admin.site.register(Patient)
 admin.site.register(Appointment)
