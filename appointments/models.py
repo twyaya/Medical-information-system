951bd596d0913b468f23855cf314e615da9d9c74
twyaya <otis8496@gmail.com>
2024-10-27 21:10:27 +0800

    病患醫生掛號關聯化

--- appointments/models.py
+++ appointments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Patient(models.Model):
+    patient_id = models.CharField(max_length=8, unique=True, null=True, blank=True)
     name = models.CharField(max_length=100)
     dob = models.DateField()  # Date of birth
     phone = models.CharField(max_length=15)
@@ -9,12 +10,15 @@ class Patient(models.Model):
 
 
 class Doctor(models.Model):
+    doctor_id = models.CharField(max_length=8, unique=True, null=True, blank=True)
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
     age = models.IntegerField(null=False, default=18)
 
 
 class Appointment(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_id = models.CharField(max_length=8, unique=True, null=True, blank=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, db_column="patient_id")
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, db_column="doctor_id")
     date = models.DateTimeField()
     description = models.TextField()
