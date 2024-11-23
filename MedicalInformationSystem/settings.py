6647e4012ef9b437c0a3bc7c687701f784adcd06
twyaya <otis8496@gmail.com>
2024-11-23 20:17:16 +0800

    billing使用基底模版

--- MedicalInformationSystem/settings.py
+++ MedicalInformationSystem/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = 'static/'  # 靜態文件的 URL 前綴
+STATIC_URL = '/static/'  # 靜態文件的 URL 前綴
 
 # 設置全局靜態文件目錄
 STATICFILES_DIRS = [
